8c21f13956a4882462ecfc0d4469b559314727c3
Szymon Kurcab <szymon.kurcab@gmail.com>
2024-01-22 08:58:23 -0500

    Working version

--- function_call_with_streaming.py
+++ function_call_with_streaming.py
@@ -10,7 +10,7 @@ def get_current_weather(location, unit="fahrenheit"):
     if "tokyo" in location.lower():
         return json.dumps({"location": "Tokyo", "temperature": "10", "unit": unit})
     elif "san francisco" in location.lower():
-        return json.dumps({"location": "San Francisco", "temperature": "72", "unit": unit})
+        return json.dumps({"location": "San Francisco", "temperature": "32", "unit": unit})
     elif "paris" in location.lower():
         return json.dumps({"location": "Paris", "temperature": "22", "unit": unit})
     else:
@@ -18,7 +18,7 @@ def get_current_weather(location, unit="fahrenheit"):
 
 def run_conversation():
     # Step 1: send the conversation and available functions to the model
-    messages = [{"role": "user", "content": "What's the weather like in San Francisco, Tokyo, and Paris?"}]
+    messages = [{"role": "user", "content": "Tell me a joke and then tell me what's the weather like in San Francisco, Tokyo, and Paris?"}]
     tools = [
         {
             "type": "function",
@@ -39,44 +39,82 @@ def run_conversation():
             },
         }
     ]
-    response = client.chat.completions.create(
-        model="gpt-3.5-turbo-1106",
+    stream = client.chat.completions.create(
+        model="gpt-4-1106-preview",
         messages=messages,
         tools=tools,
         tool_choice="auto",  # auto is default, but we'll be explicit
-        #stream=True,
-    )
-    response_message = response.choices[0].message
-    tool_calls = response_message.tool_calls
-    # Step 2: check if the model wanted to call a function
-    if tool_calls:
-        # Step 3: call the function
-        # Note: the JSON response may not always be valid; be sure to handle errors
-        available_functions = {
-            "get_current_weather": get_current_weather,
-        }  # only one function in this example, but you can have multiple
-        messages.append(response_message)  # extend conversation with assistant's reply
-        # Step 4: send the info for each function call and function response to the model
-        for tool_call in tool_calls:
-            function_name = tool_call.function.name
-            function_to_call = available_functions[function_name]
-            function_args = json.loads(tool_call.function.arguments)
-            function_response = function_to_call(
-                location=function_args.get("location"),
-                unit=function_args.get("unit"),
-            )
-            messages.append(
-                {
-                    "tool_call_id": tool_call.id,
-                    "role": "tool",
-                    "name": function_name,
-                    "content": function_response,
-                }
-            )  # extend conversation with function response
-        second_response = client.chat.completions.create(
-            model="gpt-3.5-turbo-1106",
-            messages=messages,
-        )  # get a new response from the model where it can see the function response
-        return second_response
-print(run_conversation())
+        stream=True,
+    ) 
 
+    available_functions = {
+        "get_current_weather": get_current_weather,
+    }  # only one function in this example, but you can have multiple
+
+    response_text = ""
+    tool_calls = []
+
+    for chunk in stream:
+        delta = chunk.choices[0].delta
+        # print(delta)
+
+        if delta and delta.content:
+            # content chunk -- send to browser and record for later saving
+            print(delta.content)
+            response_text += delta.content
+
+        elif delta and delta.tool_calls:
+            tcchunklist = delta.tool_calls
+            for tcchunk in tcchunklist:
+                if len(tool_calls) <= tcchunk.index:
+                    tool_calls.append({"id": "", "type": "function", "function": { "name": "", "arguments": "" } })
+                tc = tool_calls[tcchunk.index]
+
+                if tcchunk.id:
+                    tc["id"] += tcchunk.id
+                if tcchunk.function.name:
+                    tc["function"]["name"] += tcchunk.function.name
+                if tcchunk.function.arguments:
+                    tc["function"]["arguments"] += tcchunk.function.arguments    
+
+    # print(tool_calls)
+
+    messages.append(
+        {
+            "tool_calls": tool_calls,
+            "role": 'assistant',
+        }                    
+    )    
+
+    for tool_call in tool_calls:
+        function_name = tool_call['function']['name']
+        function_to_call = available_functions[function_name]
+        function_args = json.loads(tool_call['function']['arguments'])
+        function_response = function_to_call(
+            location=function_args.get("location"),
+            unit=function_args.get("unit"),
+        )
+        messages.append(
+            {
+                "tool_call_id": tool_call['id'],
+                "role": "tool",
+                "name": function_name,
+                "content": function_response,
+            }
+        )  # extend conversation with function response
+    
+    # print(messages)
+    
+    stream = client.chat.completions.create(
+        model="gpt-4-1106-preview",
+        messages=messages,
+        stream=True,
+    ) 
+
+    for chunk in stream:
+        if chunk.choices[0].delta.content is not None:
+            delta = chunk.choices[0].delta
+            print(delta.content, end="")        
+            response_text += delta.content
+
+run_conversation()
